5563267758d465b0a900bc0f81c98e638da7e78d
Architect Salvatore IV <architect-iv@aos-core.com>
2026-01-19 13:48:54 -0500

    Add Plausible Analytics for unified Cathedral dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logo-git-is-life.png" type="image/png" />
+        <script defer data-domain="git-islife.com" src="https://plausible.io/js/script.js"></script>
       </head>
       <body>{children}</body>
     </html>
